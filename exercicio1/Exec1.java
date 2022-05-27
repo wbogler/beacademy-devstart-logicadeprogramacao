549b09c2d845288012006a0a0e1b6c08cc30ec5f
Willian Bogler Da Silva <willian.silva@pti.org.br>
2022-05-27 00:49:18 -0300

    Resolução exercicio 1

---
+++ exercicio1/Exec1.java
@@ -0,0 +1,22 @@
+package paylivre.exercicios;
+
+public class Exec1 {
+	
+	private String nome;
+	private float peso;
+	private float altura;
+	
+	public Exec1(String nome, float peso, float altura) {
+		super();
+		this.nome = nome;
+		this.peso = peso;
+		this.altura = altura;
+	}
+	
+	public void dados() {
+		System.out.println("nome: " + nome);
+		System.out.println("peso: " + peso+ " Kg");
+		System.out.println("altura: " + altura+ " m");
+	}
+
+}
---
+++ exercicio1/main.java
@@ -0,0 +1,12 @@
+package paylivre.exercicios;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Exec1 exercicio1 = new Exec1("Willian", 120, 180);
+		exercicio1.dados();
+
+	}
+
+}
